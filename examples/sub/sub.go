52af0974054dfaa90eaa192cbecb2241bffd69a0
Chris Lu <chris.lu@gmail.com>
2020-04-18 16:06:00 -0700

    add examples

---
+++ examples/sub/sub.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/chrislusf/seaweedfs/weed/messaging/client"
+	"github.com/chrislusf/seaweedfs/weed/pb/messaging_pb"
+)
+
+func main() {
+	mc, err := client.NewMessagingClient([]string{"localhost:9777"})
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+	defer mc.Shutdown()
+
+	sub, err := mc.NewSubscriber("subscriber1", "ns1", "topic1")
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+	defer sub.Shutdown()
+
+	sub.Subscribe(func(m *messaging_pb.Message) {
+		fmt.Printf("> %s\n", string(m.Value))
+	})
+}
